3ac27e34f15c8652434285a3d33625e82cc2a670
Your Name <you@example.com>
2024-09-10 11:07:33 +0700

    new file:   JS3/controlFlow.dart

---
+++ JS3/controlFlow.dart
@@ -0,0 +1,18 @@
+void main(){
+
+// langkah 1
+  String test = "test2";
+
+if (test == "test1") {
+   print("Test1");
+} else if(test == "test2") {
+   print("Test2");
+} else {
+   print("Something else");
+}
+
+if (test == "test2") print("Test2 again");
+
+// langkah 3
+
+}
