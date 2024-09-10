7ed9b2a3ff22549725ccca78bd0860f61d30c214
Your Name <you@example.com>
2024-09-10 11:29:58 +0700

    JS 3 - Prakitkum 2

---
+++ JS3/looping.dart
@@ -0,0 +1,15 @@
+void main() {
+// Langkah 1
+  // int counter = 0;
+  // while (counter < 33) {
+  //   print(counter);
+  //   counter++;
+  // }
+
+  // langkah 3
+  int counter = 50;
+  do {
+    print(counter);
+    counter++;
+  } while (counter < 77);
+}
