5a72147f5ebb5422c7b7966330b45a60caefc5b8
Your Name <you@example.com>
2024-09-10 11:18:38 +0700

    JS 3 - Praktikum 1

--- JS3/controlFlow.dart
+++ JS3/controlFlow.dart
@@ -1,18 +1,23 @@
 void main(){
 
 // langkah 1
-  String test = "test2";
 
-if (test == "test1") {
-   print("Test1");
-} else if(test == "test2") {
-   print("Test2");
-} else {
-   print("Something else");
-}
+//   String test = "test2";
 
-if (test == "test2") print("Test2 again");
+// if (test == "test1") {
+//    print("Test1");
+// } else if(test == "test2") {
+//    print("Test2");
+// } else {
+//    print("Something else");
+// }
+
+// if (test == "test2") print("Test2 again");
 
 // langkah 3
+String test = "true";
+if (test == "true") {
+   print("Kebenaran");
+}
 
 }
