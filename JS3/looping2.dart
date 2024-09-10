aedfb5b12df53b754a9833a353bc8e72355da5ea
Your Name <you@example.com>
2024-09-10 11:46:15 +0700

    JS 3 - Praktikum 3

---
+++ JS3/looping2.dart
@@ -0,0 +1,24 @@
+void main(List<String> args) {
+  
+  // // langkah 1
+  // int index;
+  // for (index = 10; index < 27; index++) {
+  //   print(index);
+  // }
+
+  // langkah 3
+int Index;
+
+for (Index = 0; Index < 30; Index++) {
+  if (Index == 21) {
+    break;
+  }
+  else if (Index > 1 && Index < 7) {
+    continue;
+    
+  } 
+ print(Index);
+ 
+}
+
+}
